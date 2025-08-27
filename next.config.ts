c91703f2f8ae1fce85161fe8eeae23cf0cf650ef
wjwu <cs11b137@gmail.com>
2025-08-27 13:35:36 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,27 @@
 import type { NextConfig } from "next"
 
+// 判断是否在 GitHub Pages 部署
+const isProd = process.env.NODE_ENV === "production"
+
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
-  experimental: {},
+  output: "export", // 👈 让 Next.js 生成静态文件
   images: {
+    unoptimized: true, // 👈 GitHub Pages 不支持 Next.js 图片优化
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**"
-      }
-    ]
+        hostname: "**",
+      },
+    ],
   },
   compiler: {
-    removeConsole: true
-  }
+    removeConsole: true,
+  },
+  // 👇 basePath & assetPrefix 用于 GitHub Pages 子路径
+  basePath: isProd ? "/next-web-nav" : "",
+  assetPrefix: isProd ? "/next-web-nav/" : "",
 }
 
 export default nextConfig
+
